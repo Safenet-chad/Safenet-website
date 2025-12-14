c8492a54eee5d38cdf56e5000366c73452f953f4
Safenet-chad <safenetchad@gmail.com>
2025-12-14 17:28:02 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,5 @@
-
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+menuBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
